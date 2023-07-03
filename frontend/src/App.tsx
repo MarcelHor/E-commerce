b3f136a57fc14068c30c486bd8755b20cace626c
Marcel_Horvath <marcel.horvath@tul.cz>
2023-07-03 22:35:56 +0200

    no auth for me

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,12 @@ import Header from "./components/Header.tsx";
 import ProductList from "./pages/ProductList.tsx";
 import CartCheckout from "./pages/CartCheckout.tsx";
 import CartProvider from "./context/CartProvider.tsx";
-import Register from "./pages/Register.tsx";
-import Login from "./pages/Login.tsx";
-import PrivateRoute from "./utils/PrivateRoute.tsx";
-import AuthProvider from "./context/AuthProvider.tsx";
-import Profile from "./pages/Profile.tsx";
+
 
 export default function App() {
     return (
         <>
             <Router>
-                <AuthProvider>
                     <CartProvider>
                         <Header/>
                         <Routes>
@@ -23,13 +18,9 @@ export default function App() {
                             <Route path="/products/:category/:slug" element={<ProductDetail/>}/>
                             <Route path="/products/:category" element={<ProductList/>}/>
                             <Route path="/cart" element={<CartCheckout/>}/>
-                            <Route path="/register" element={<Register/>}/>
                             <Route path="*" element={<h1>Not Found</h1>}/>
-                            <Route path={"/login"} element={<Login/>}/>
-                            <Route path={"/profile"} element={<PrivateRoute><Profile/></PrivateRoute>}/>
                         </Routes>
                     </CartProvider>
-                </AuthProvider>
             </Router>
         </>
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,11 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faShoppingCart, faUser} from '@fortawesome/free-solid-svg-icons';
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 import {Link} from "react-router-dom";
 import SearchBar from "./SearchBar.tsx";
 import {useCart} from "../context/CartProvider.tsx";
-import {useAuth} from "../context/AuthProvider.tsx";
 
 export default function Header() {
     const {getCartCount} = useCart();
-    const {user, logoutUser} = useAuth();
 
     return (
         <>
@@ -21,24 +19,6 @@ export default function Header() {
                     <SearchBar/>
 
                     <div className="flex items-center space-x-4">
-                        <div className="flex items-center space-x-4">
-                            {user ? (
-                                <Link to={"/profile"} className="text-xl space-x-2 flex items-center">
-                                    <FontAwesomeIcon icon={faUser} className="text-2xl"/>
-                                    <span className="text-xl underline">{user.email}</span>
-                                </Link>
-                            ) : (
-                                <Link to={"/login"} className="text-xl">Login</Link>
-                            )}
-
-                            {user ? (
-                                <button onClick={logoutUser}
-                                        className="text-l bg-red-500 text-white rounded p-2">Logout</button>
-                            ) : (
-                                <Link to={"/register"} className="text-xl">Register</Link>
-                            )}
-                        </div>
-
                         <Link className="text-white text-xl px-4 py-2 rounded-md flex items-center" to={"/cart"}>
                             <FontAwesomeIcon icon={faShoppingCart} color={"black"} className="text-2xl"/>
                             <span className="bg-red-500 text-white rounded-full px-2 py-1 text-xs font-bold ml-2">
